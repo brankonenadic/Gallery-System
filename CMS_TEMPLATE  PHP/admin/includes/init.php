21646b224fb6122b80cd473dfde5d24dcac6ec3e
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-09-07 23:49:15 +0200

    New

---
+++ CMS_TEMPLATE  PHP/admin/includes/init.php
@@ -0,0 +1,3 @@
+<?php
+include('new_confing.php');
+?>
---
+++ CMS_TEMPLATE  PHP/admin/index.php
@@ -0,0 +1,16 @@
+<?php include("includes/header.php"); ?>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <?php include("includes/topNav.php"); ?>    
+        <?php include("includes/sidebar.php"); ?> 
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+        <?php include("includes/adminContect.php"); ?>
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include("includes/footer.php"); ?>
